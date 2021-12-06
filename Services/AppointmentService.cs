a915719ed93fa5e5304a822bdeb00c3d5f767db6
minnasydan <minnasydan@gmail.com>
2021-12-06 17:42:21 +0200

    Initial commit

---
+++ Services/AppointmentService.cs
@@ -0,0 +1,16 @@
+﻿using AppointmentScheduling.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Services
+{
+    public class AppointmentService : IAppointmentService
+    {
+        public List<DoctorVM> doctorVMs()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/IAppointmentService.cs
@@ -0,0 +1,13 @@
+﻿using AppointmentScheduling.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Services
+{
+    public interface IAppointmentService
+    {
+        public List<DoctorVM> doctorVMs();
+    }
+}
